80ae3f25e05ecd708f92ef629f0bd94fe8213b54
Kevin Lewis <KLewis@clevelandbrowns.com>
2021-07-15 09:12:58 -0400

    started implementing analytics tab

---
+++ ShotTracker/ShotTracker/ViewModels/ChartViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microcharts;
+using ShotTracker.Models;
+using SkiaSharp;
+using Entry = Microcharts.ChartEntry;
+
+namespace ShotTracker.ViewModels
+{
+    public class ChartViewModel : BaseViewModel
+    {
+        private List<ShotEntry> _shotEntries;
+        public ChartViewModel()
+        {
+            Title = "Analytics";
+            Task.Run(() => LoadShotEntries());
+            
+        }
+
+        public List<ShotEntry> ShotEntries
+        {
+            get
+            {
+                return _shotEntries;
+            }
+            set
+            {
+                _shotEntries = value;
+                OnPropertyChanged(nameof(ShotEntries));
+            }
+        }
+
+        public List<Entry> GetChartProgressDailyEntries()
+        {
+            DateTime day1 = new DateTime(DateTime.Today.Ticks);
+            float val1 = (float)(ShotEntries.Where(o => (o.Date >= day1)).ToList().Count > 0 ? Math.Round((float)ShotEntries.Where(o => o.Date >= day1).Sum(item => item.Makes) / (float)ShotEntries.Where(o => o.Location == ShotLocation.Paint).Sum(item => item.Makes + item.Misses) * 100) : 0);
+            List<Entry> entries = new List<Entry>
+            {             
+                new Entry(val1)
+                {
+                    Color=SKColor.Parse("#FF1943"),
+                    Label = (DateTime.Now.DayOfWeek - 4).ToString(),
+                    ValueLabel = "200"
+                },
+                new Entry(400)
+                {
+                    Color = SKColor.Parse("00BFFF"),
+                    Label = (DateTime.Now.DayOfWeek - 3).ToString(),
+                    ValueLabel = "400"
+                },
+                new Entry(-100)
+                {
+                    Color =  SKColor.Parse("#00CED1"),
+                    Label = (DateTime.Now.DayOfWeek - 2).ToString(),
+                    ValueLabel = "-100"
+                },
+                new Entry(-100)
+                {
+                    Color =  SKColor.Parse("#00CED1"),
+                    Label = (DateTime.Now.DayOfWeek - 1).ToString(),
+                    ValueLabel = "-100"
+                },
+                new Entry(-100)
+                {
+                    Color =  SKColor.Parse("#00CED1"),
+                    Label = (DateTime.Now.DayOfWeek - 0).ToString(),
+                    ValueLabel = "-100"
+                },
+            };
+            return entries;
+        }
+
+        private async void LoadShotEntries()
+        {
+            try
+            {
+                ShotEntries.Clear();
+                var items = await DataStore.GetShotEntriesAsync(true);
+                foreach (var item in items)
+                {
+                    ShotEntries.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ ShotTracker/ShotTracker/Views/ChartPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microcharts;
+using ShotTracker.Services;
+using ShotTracker.ViewModels;
+using SkiaSharp;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Entry = Microcharts.ChartEntry;
+
+namespace ShotTracker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChartPage : ContentPage
+    {
+        ChartViewModel _viewModel;
+        public ChartPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ChartViewModel();
+            ChartProgressDaily.Chart = new LineChart() { Entries = _viewModel.GetChartProgressDailyEntries() };
+        }       
+    }
+}
